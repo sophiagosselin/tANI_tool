bff798a65788f098f4bc5488bb03258698dcce64
SeanGosselin <44369900+SeanGosselin@users.noreply.github.com>
2020-01-21 15:52:48 -0500

    Update tANI_Open_Beta.pl

--- tANI_Open_Beta.pl
+++ tANI_Open_Beta.pl
@@ -23,9 +23,9 @@ if (!exists $ARGV[0]){
 	die "\nNo inputs detected, please use Default, or type -H for assistance.\n\n";
 }
 elsif ($ARGV[0] eq "-H"){
-	die "\nThe following options are available:\n\n\t-ID:# - The identity cutoff value (0-100).\n\t-CV:# - The coverage cutoff value (0-100).\n\t-BT:# - The number of nonparametric bootstraps for tree building (0-n).\n\t-EVAL:# - Change E-Value cutoff of BLAST search.\n\t-TASK:'task name' If you wish to change which default settings BLAST uses for its search criteria.\n\tDefault - Uses default search criteria as described in Gosselin et al. 2019.\n\nIf you encounter negative values in your matrix, or critical errors send an email to seangosselinofficial\@gmail.com. I will try to answer in a reasonable fassion.\n\n\n";
+	die "\nThe following options are available:\n\n\t-ID:# - The identity cutoff value (0-100).\n\t-CV:# - The coverage cutoff value (0-100).\n\t-BT:# - The number of nonparametric bootstraps for tree building (0-n).\n\t-EVAL:# - Change E-Value cutoff of BLAST search.\n\t-TASK:'task name' If you wish to change which default settings BLAST uses for its search criteria.\n\tD - Uses default search criteria as described in Gosselin et al. 2019.\n\nIf you encounter negative values in your matrix, or critical errors send an email to seangosselinofficial\@gmail.com. I will try to answer in a reasonable fassion.\n\n\n";
 }
-elsif ($ARGV[0] eq "Default"){
+elsif ($ARGV[0] eq "-D"){
 	$identity = .7;
 	$coverage = .7;
 	$bootnum = 100;
